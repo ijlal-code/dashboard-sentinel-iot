c5525fef4782d7c636bc262e6d7202a55b7d2afb
Ainun Ijlal <ijlalcode@gmail.com>
2026-04-15 18:24:11 +0800

    coba

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,3 @@ Route::get('/dashboard', function () {
     return view('monitoring');
 });
 
-// Route /kirim-data, /monitoring, dan /buzzer/toggle SUDAH DIHAPUS dari sini 
-// karena sudah dipindahkan ke rute yang benar yaitu routes/api.php
